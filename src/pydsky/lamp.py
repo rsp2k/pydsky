fda5a2c61b4cefee362a4f6219f98ea8fe7da052
Ryan Malloy <ryan@supported.systems>
2026-02-19 14:08:10 -0700

    Add 3-bulb hotspot effect and correct lamp sprite colors
    Lamp widget now renders multiple overlapping radial gradients to
    simulate the uneven glow of incandescent indicator bulbs. Lamp
    sprites updated with corrected white/yellow tint.

--- src/pydsky/lamp.py
+++ src/pydsky/lamp.py
@@ -1,8 +1,9 @@
 from qtpy.QtWidgets import QWidget
-from qtpy.QtGui import QPainter
+from qtpy.QtGui import QPainter, QRadialGradient, QColor, QBrush
+from qtpy.QtCore import QPointF
 
 class Lamp(QWidget):
-    def __init__(self, parent, pix, x, y, w, h, on):
+    def __init__(self, parent, pix, x, y, w, h, on, bulbs=0):
         super().__init__(parent)
         self._pix = pix
         self._x = x
@@ -10,6 +11,7 @@ class Lamp(QWidget):
         self._w = w
         self._h = h
         self._on = on
+        self._bulbs = bulbs
         self._setup_ui()
         self._flash = False
 
@@ -31,3 +33,22 @@ class Lamp(QWidget):
         p = QPainter(self)
         if self._on and not self._flash:
             p.drawPixmap(0, 0, self._pix, self._x, self._y, self._w, self._h)
+            if self._bulbs > 0:
+                self._draw_bulb_hotspots(p)
+
+    def _draw_bulb_hotspots(self, p):
+        p.setRenderHint(QPainter.RenderHint.Antialiasing)
+        cy = self._h / 2.0
+        radius = self._h * 0.55
+        spacing = self._w / (self._bulbs + 1)
+
+        for i in range(self._bulbs):
+            cx = spacing * (i + 1)
+            gradient = QRadialGradient(QPointF(cx, cy), radius)
+            gradient.setColorAt(0.0, QColor(255, 255, 255, 160))
+            gradient.setColorAt(0.3, QColor(255, 255, 255, 80))
+            gradient.setColorAt(0.7, QColor(255, 255, 255, 20))
+            gradient.setColorAt(1.0, QColor(255, 255, 255, 0))
+            p.setBrush(QBrush(gradient))
+            p.setPen(QColor(0, 0, 0, 0))
+            p.drawEllipse(QPointF(cx, cy), radius, radius)
